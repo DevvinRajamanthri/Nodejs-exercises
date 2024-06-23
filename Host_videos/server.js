590802727c1bf04ae363bdf4db0a79d04c7d3c1e
Devvin Rajamanthri <120432493+DevvinRajamanthri@users.noreply.github.com>
2024-06-22 20:08:24 -0700

    git committing

--- Host_videos/server.js
+++ Host_videos/server.js
@@ -1,26 +1,24 @@
 var http = require('http');
 var fs = require('fs');
 let url = require('url');
-http.createServer(function(req,res){
-      //url = /video/1.mp4
-      //url = /video/2.mp4
+http.createServer(function(req, res){
       if(req.url.includes("videos")){
-            //route = ./video/2.mp4
-            let filename = "." + req.url;
-            //filename = ./video/2.mp4
-            if (fs.existsSync(filename)) {
-                  fs.readFile(filename, function(err, data) {
-                        res.writeHead(200,{'Content-Type':'video/mp4'});
+            let filename = "./t2" + req.url
+            if(fs.existsSync(filename)){
+                  fs.readFile(filename, function(err, data){
+                        res.writeHead(200, {"Content-Type" : "video/mp4"});
                         res.write(data);
-                        res.end(); 
-                  });  
+                        res.end();
+                  })
+            } else { 
+                  res.writeHead(200, {"Content-Type":"text/html"});
+                  res.end("video not foudn");
             }
-      
       } else {
-            fs.readFile('home_page.html', function(err, data) {
-                  res.writeHead(200,{'Content-Type':'text/html'});
-                  res.write(data);
-                  res.end(); 
-            });
+           fs.readFile("home_page.html", function(err, data){
+             res.writeHead(200, {"Content-Type":"text/html"});
+             res.write(data);
+             res.end();
+           });
       }
-}).listen(8080);
+}).listen(8080)
--- test/server.js
+++ test/server.js
@@ -1,5 +1,8 @@
 let http = require("http");
+let url =  require("url")
 http.createServer(function(req, res){
+    let child = url.parse(req.url, false);
+  
     res.writeHead(200, {"Content-Type":"text/html"});
     res.end("hello_world")
 }).listen(8080);
