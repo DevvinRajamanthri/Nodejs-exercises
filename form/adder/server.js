58388e7888dbbe033c31a78ca51dc813ec9edb7c
Devvin Rajamanthri <120432493+DevvinRajamanthri@users.noreply.github.com>
2024-07-08 14:24:07 -0700

    adding two mumbers

---
+++ form/adder/server.js
@@ -0,0 +1,23 @@
+let express = require("express");
+let app = express();
+let fs = require("fs");
+app.use(express.urlencoded({ extended: true })); 
+app.get("/", function(req, res){
+    fs.readFile("form.html", function(err, data){
+        res.write(data);
+        res.end();
+    });
+})
+app.post('/submit', (req, res) => {
+    let numbers1 = req.body.n1;
+    let numbers2 = req.body.n2;
+    numbers1 = parseInt(numbers1)
+    numbers2 = parseInt(numbers2)
+    let sum = numbers1 + numbers2
+    sum = sum.toString();
+    res.send(sum);
+  });
+  
+      
+    
+app.listen(8080);
